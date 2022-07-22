1c5ec7def9c7d15fce5f3522f9def969335b7b3f
Edwin Codrington <edwincodrington@gmail.com>
2022-07-21 20:37:33 -0400

    dummy push for heroku and git

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, response) => {
+	response.send("Home");
+});
+
+app.get("*", (request, response) => {
+	response.status(404).send("Sorry, no page found!");
+});
+
+module.exports = app;
