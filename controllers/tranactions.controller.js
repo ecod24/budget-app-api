6f6c1dcfc22d53ab4b3d44171b23610c3a20bbab
Edwin Codrington <edwincodrington@gmail.com>
2022-07-25 11:28:01 -0400

    update to backend routes

--- controllers/tranactions.controller.js
+++ controllers/tranactions.controller.js
@@ -8,29 +8,32 @@ transactions.use("/:id", (request, response, next) => {
 	}
 	next();
 });
-
+//READ
 transactions.get("/", (request, response) => {
 	console.log("GET REQUESTED");
 	response.json(transactionsData);
 });
-
+//CREATE
 transactions.post("/", (request, response) => {
 	console.log("POST REQUESTED");
 	transactionsData.push(request.body);
 	response.json(transactionsData[transactionsData.length - 1]);
 });
-
+//READ/:INDEX
 transactions.get("/:id", (request, response) => {
 	console.log(`GET/${request.params.id} REQUESTED`);
 	response.json(transactionsData[request.params.id]);
 });
 
+//UPDATE
 transactions.put("/:id", (request, response) => {
-	//TODO implement: do "insert" instead of replace if  entry exists at spot
-	console.log(`PUT/${request.params.id} REQUESTED`);
-	transactionsData[Number(request.params.id)] = request.body;
-	response.json(transactionsData[Number(request.params.id)]);
+	let { id } = request.params;
+	id = Number(id);
+	console.log(`PUT/${id} REQUESTED`);
+	transactionsData[id] = request.body;
+	response.json(transactionsData[id]);
 });
+//DELETE
 transactions.delete("/:id", (request, response) => {
 	console.log(`DELETE/${request.params.id} REQUESTED`);
 	transactionsData.splice(Number(request.params.id), 1);
--- models/transaction.model.js
+++ models/transaction.model.js
@@ -5,4 +5,10 @@ module.exports = [
 		transactionName: "Google Pixel 4a Unlocked",
 		amount: 59.99,
 	},
+	{
+		date: "02-15-22",
+		merchant: "Amazon",
+		transactionName: "MacBook Pro 14-inch with M1 Pro",
+		amount: 1785.33,
+	},
 ];
