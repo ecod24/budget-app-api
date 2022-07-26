9252b595a7adcf777dc74d4c73d3ae67265cab2b
Edwin Codrington <edwincodrington@gmail.com>
2022-07-26 00:00:27 -0400

    fixed data model to use proper object keys

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,14 +1,16 @@
 module.exports = [
 	{
 		date: "07-22-22",
-		merchant: "eBay",
-		transactionName: "Google Pixel 4a Unlocked",
+		from: "eBay",
+		item_name: "Google Pixel 4a Unlocked",
 		amount: 59.99,
+		category: "Online Shopping",
 	},
 	{
 		date: "02-15-22",
-		merchant: "Amazon",
-		transactionName: "MacBook Pro 14-inch with M1 Pro",
+		from: "Amazon",
+		item_name: "MacBook Pro 14-inch with M1 Pro",
 		amount: 1785.33,
+		category: "Online Shopping",
 	},
 ];
